c2a395133e38b983317b36dc766dda1e21ac699b
nfernando0 <nandof986@gmail.com>
2023-03-16 04:10:19 +0700

    merubah navbar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ const navbarNav = document.querySelector(".navbar__nav");
 document.querySelector("#hamb-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+const hamb = document.querySelector("#hamb-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamb.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
